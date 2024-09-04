6c5551794b9c8cf72abff8a330ecb0e4efada811
maishamir <45680002+maishamir@users.noreply.github.com>
2024-09-04 14:44:12 -0700

    added logos; needs styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Tools from "./components/Tools/Tools";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <Hero />
         <About />
+        <Tools />
       </div>
     </>
   );
