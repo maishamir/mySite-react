84dfd59c65bc3b023f2f3f790d36d0451c021c86
maishamir <45680002+maishamir@users.noreply.github.com>
2024-09-03 17:25:59 -0700

    added arrow for project section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,11 +5,17 @@ import "./Hero.scss";
 function Hero() {
   return (
     <main className="hero">
-      <img src={heroImg} className="hero__img" alt="img of maisha mir" />
-      <div>
-        <h1>HI! I'M MAISHA MIR</h1>
-        <p>I'm a fullstack developer based in Victoria, BC.</p>
-      </div>
+      <div className="hero__content">
+        <img src={heroImg} className="hero__img" alt="img of maisha mir" />
+        <div>
+          <h1>HI! I'M MAISHA MIR</h1>
+          <p>I'm a fullstack developer based in Victoria, BC.</p>
+        </div>
+          </div>
+          
+          <div className="hero__arrow">
+              V
+          </div>
     </main>
   );
 }
