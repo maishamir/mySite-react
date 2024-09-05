6dc36abb50790e412ef1c1e1990e29e76942ed03
maishamir <45680002+maishamir@users.noreply.github.com>
2024-09-05 14:39:29 -0700

    basic project card; minimal styling

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import projImage from "../../assets/images/Spell Randomizer.png"
+import "./Project.scss"
 
 function Project() {
   return (
     <section className="project">
           <div className="project__container">
-              <div className="project__screenshot">
-                  <img src={projImage} alt="" />
+              <div className="project__img-container">
+                  <a href="https://wand-and-couldron.up.railway.app/"><img src={projImage} alt="" className="project__img"/></a> 
               </div>
               <div className="project__description">
-                  <p>Project Name</p>
-                  <p>This is the description of the project including the api(s) I used and blah blah.</p>
+                  <p>Harry Potter Spell Randomizer</p>
+                  <p>A whismical Harry Potter Spell randomizer which uses the PotterDB ApI to generate and display random spells from the Harry Potter universe.</p>
                   <div className="project__links">
-                      <a href="#">Demo</a>
-                      <a href="#">Github</a>
+                      <a href="https://github.com/maishamir/hp-spell-randomizer">Github</a>
                   </div>
                   <div className="project__tech">
-                      list | of | tech | used
+                      CSS | JavaScript | EJS 
                   </div>
               </div>
           </div>
