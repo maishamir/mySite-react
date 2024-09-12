1a31589bae1be16e4489f0b012d9f3ea228f4c0d
maishamir <45680002+maishamir@users.noreply.github.com>
2024-09-11 17:34:17 -0700

    changed hero and tools styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import heroImg from "../../assets/images/Maisha Mir_LI.jpg";
+import arrow from "/Users/maishamir/Documents/mySite-react/public/icons/arrow button circle.svg"
 import "./Hero.scss";
 
 function Hero() {
   return (
     <main className="hero">
       <div className="hero__content">
-        <img src={heroImg} className="hero__img" alt="img of maisha mir" />
-        <div>
-          <h1>HI! I'M MAISHA MIR</h1>
+        {/* <img src={heroImg} className="hero__img" alt="img of maisha mir" /> */}
+        <div className="hero__text">
+          <h1 className="hero__title">HEY I'M MAISHA</h1>
           <p>I'm a fullstack developer based in Victoria, BC.</p>
         </div>
           </div>
           
           <div className="hero__arrow">
-              <a href="#about-me">V</a>
+              <a href="#projects"><img className="hero__icon" src={arrow}/></a>
           </div>
     </main>
   );
